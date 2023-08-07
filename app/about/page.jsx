e17e3f1e6524e87b2d047d4516c4629633359e9d
Khaleda Begum <khaledab.work@gmail.com>
2023-08-07 12:58:06 +0100

    "Initial commit"

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,9 +4,8 @@ export default function page() {
   return (
     <div>
       <h1 className='text-red-600 text-2xl'>
-        This is Home page
+        This is About page
       </h1>
-      <img src="/images/shoppingbags.jpg"/>
     </div>
   )
 }
---
+++ app/product/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className='text-green-600 text-2xl'>
+        This is Product page
+      </h1>
+    </div>
+  )
+}
